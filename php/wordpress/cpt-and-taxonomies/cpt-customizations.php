fdc15f38a5ff374df160c6603daf935ac14c90e7
David Wood <dfwood90@gmail.com>
2014-04-29 15:50:14 -0400

    Added filter for post update text.

--- php/wordpress/cpt-and-taxonomies/cpt-customizations.php
+++ php/wordpress/cpt-and-taxonomies/cpt-customizations.php
@@ -49,7 +49,6 @@ add_action( "manage_{$post_type}_posts_custom_column", '_manage_custom_column', 
  * Allows our custom columns to become sortable
  * @param array $columns
  * @return array
- * @since 0.8.0
  */
 function _manage_sortable_columns( $columns ) {
     $columns['my_column'] = 'sort_by_me!';
@@ -57,3 +56,33 @@ function _manage_sortable_columns( $columns ) {
     return $columns;
 }
 add_filter( "manage_edit-{$post_type}_sortable_columns", '_manage_sortable_columns' );
+
+
+/**
+ * Allows customization of post updated messages for specific post types
+ * @param $messages
+ * @return mixed
+ */
+function _filter_post_type_updated_messages( $messages ) {
+    global $post, $post_ID;
+
+    $messages['book_cpt'] = array(
+        0 => '', // Unused, messages start with index 1
+        1 => sprintf( __( 'Book updated. <a href="%s">View book</a>' ), esc_url( get_permalink( $post_ID ) ) ),
+        2 => __( 'Custom field updated.' ),
+        3 => __( 'Custom field deleted.' ),
+        4 => __( 'Book updated.' ),
+        /* translators: %s: date and time of the revision */
+        5 => isset( $_GET['revision'] ) ? sprintf( __( 'Book restored to revision from %s' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+        6 => sprintf( __( 'Book published. <a href="%s">View book</a>' ), esc_url( get_permalink( $post_ID ) ) ),
+        7 => __( 'Book saved.' ),
+        8 => sprintf( __( 'Book submitted. <a target="_blank" href="%s">Preview book</a>' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
+        9 => sprintf( __( 'Book scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview book</a>' ),
+            // translators: Publish box date format, see http://php.net/date
+            date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ), esc_url( get_permalink( $post_ID ) ) ),
+        10 => sprintf( __( 'Book draft updated. <a target="_blank" href="%s">Preview book</a>' ), esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) ),
+    );
+
+    return $messages;
+}
+add_filter( 'post_updated_messages', '_filter_post_type_updated_messages' );
